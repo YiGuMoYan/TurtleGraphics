e70c49ba1c2911c83c65b04f7c04a8fa84df067f
YiGuMoYan <3194775246@qq.com>
2024-01-06 15:43:06 +0800

    finish

---
+++ src/main/java/top/yigumoyan/Constant.java
@@ -0,0 +1,15 @@
+package top.yigumoyan;
+
+public class Constant {
+    public static final Double SPACING = 10.0;
+    public static final Double SCENE_WIDTH = 500.0;
+    public static final Double SCENE_HEIGHT = 500.0;
+    public static final Double PANE_WIDTH = 500.0;
+    public static final Double PANE_HEIGHT = 400.0;
+    public static final Double PADDING = 10.0;
+    public static final String UP = "抬笔";
+    public static final String DOWN = "落笔";
+    public static final String CLEAR = "清除";
+    public static final String CURRENT_STATE = "当前状态：";
+
+}
---
+++ src/main/java/top/yigumoyan/Main.java
@@ -0,0 +1,79 @@
+package top.yigumoyan;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    private boolean isDrawing = false;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox vBox = new VBox();
+        vBox.setSpacing(Constant.SPACING);
+        vBox.setPadding(new Insets(Constant.PADDING));
+
+        Canvas canvas = new Canvas(Constant.PANE_WIDTH, Constant.PANE_HEIGHT);
+
+        Button buttonUP = new Button(Constant.UP);
+        Button buttonDown = new Button(Constant.DOWN);
+        Button buttonClear = new Button(Constant.CLEAR);
+
+        HBox hBoxButton = new HBox();
+        hBoxButton.getChildren().addAll(buttonUP, buttonDown, buttonClear);
+        hBoxButton.setSpacing(Constant.SPACING);
+
+        Label label = new Label(String.format("%s%s", Constant.CURRENT_STATE, Constant.UP));
+
+        vBox.getChildren().addAll(canvas, hBoxButton, label);
+
+        Scene scene = new Scene(vBox, Constant.SCENE_WIDTH, Constant.SCENE_HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        canvas.setOnMouseDragged(event -> {
+            if (isDrawing) {
+                canvas.getGraphicsContext2D().lineTo(event.getX(), event.getY());
+                canvas.getGraphicsContext2D().stroke();
+            }
+        });
+
+        canvas.setOnMousePressed(event -> {
+            System.out.println(1);
+            canvas.getGraphicsContext2D().beginPath();
+        });
+
+        canvas.setOnMouseReleased(event -> {
+            System.out.println(2);
+            canvas.getGraphicsContext2D().closePath();
+        });
+
+        buttonClear.setOnAction(event -> {
+            canvas.getGraphicsContext2D().clearRect(0, 0, Constant.PANE_WIDTH, Constant.PANE_HEIGHT);
+        });
+
+        buttonUP.setOnAction(event -> {
+            isDrawing = false;
+            label.setText(String.format("%s%s", Constant.CURRENT_STATE, Constant.UP));
+        });
+
+        buttonDown.setOnAction(event -> {
+            isDrawing = true;
+            label.setText(String.format("%s%s", Constant.CURRENT_STATE, Constant.DOWN));
+        });
+    }
+}
